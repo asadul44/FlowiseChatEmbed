a018dcecd682f41356c3e366d091966f0afce755
Asadul islam <islamasadul327@gmail.com>
2024-01-18 19:21:29 +0600

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,7 +53,7 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={"https://bitsofts.io"}
+        href={'https://bitsofts.io'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
