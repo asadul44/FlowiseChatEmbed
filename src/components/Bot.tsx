20730cf644cf998cb055efc62e3bbf201cb1a814
Asadul islam <islamasadul327@gmail.com>
2024-01-21 00:40:30 +0600

    add new requirment

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -443,9 +443,6 @@ export const Bot = (props: BotProps & { class?: string }) => {
                 src={props.botMessage?.avatarSrc ?? 'https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'}
                 alt="Your Company"
               />
-              {/* <h2 class="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">
-                Please fill out the form below to continue chat with ai
-              </h2> */}
             </div>
 
             <div class="mt-10 sm:mx-auto w-72">
